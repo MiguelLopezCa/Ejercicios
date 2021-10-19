50c48284d6fa4239a1deb0a5c5d9cd06b5faac25
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2021-10-19 15:09:37 -0500

    Update ejercicio7.js

--- jacaScript/ejercicio7.js
+++ jacaScript/ejercicio7.js
@@ -1,7 +1,23 @@
 function analizar(){
     num1=  parseFloat(document.getElementById('numero1').value);
     num2=  parseFloat(document.getElementById('numero2').value);
-    if (Number.isInteger(num1&&Number.isInteger(num2))){
+
+    if(num1<0 ||num2<0){
+        if (Number.isInteger(num1)&&Number.isInteger(num2)){
+            document.getElementById('titulo').innerHTML='los numeros'+num1 +' y '+ num2 +' No son positivos';
+        }
+        if (Number.isInteger(num1)&& num2!=Number.isInteger(num2) ){
+
+        }
+        
+    }else{
+        if (Number.isInteger(num1&&Number.isInteger(num2)) ){
+            if(Number.isInteger(num1/num2)){
+                document.getElementById('titulo').innerHTML='El número ' + num1+' es divisible entre el número '+num2;
+            }else{
+                document.getElementById('titulo').innerHTML='El número ' + num1+' no es divisible entre el número '+num2;
+            }
+    }
 
     }
 }
