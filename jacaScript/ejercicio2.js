34844ef9df19094207a9e30a7677ae02b1908175
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2021-10-19 13:02:50 -0500

    Update ejercicio2.js

--- jacaScript/ejercicio2.js
+++ jacaScript/ejercicio2.js
@@ -1,11 +1,10 @@
 function analizar(){
  var m =(document.getElementById('cadena2').value);
  console.log(typeof m)
- if ( parseFloat(m)==Number.){
-    console.log(typeof(m));
-     
- }else {
-    console.log (typeof ( parseFloat(m)));
+ if(Number.parseFloat(m)){
+   console.log(typeof m);
+ }else{
+    console.log(typeof m);
  }
 
 }
