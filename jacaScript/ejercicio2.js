65f494e634ac062098636c8686d698e08e454e55
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2021-10-19 21:35:37 -0500

    ACT

--- jacaScript/ejercicio2.js
+++ jacaScript/ejercicio2.js
@@ -2,10 +2,10 @@ function analizar(){
  var m =(document.getElementById('cadena2').value);
 
  if( isNaN(m)==true){
-   document.getElementById('titulo')=(typeof m); 
+   document.getElementById('titulo').innerHTML=(typeof m); 
    
  }else{
-  document.getElementById('titulo')=(typeof parseFloat(m));
+  document.getElementById('titulo').innerHTML=(typeof parseFloat(m));
  }
 
 }
