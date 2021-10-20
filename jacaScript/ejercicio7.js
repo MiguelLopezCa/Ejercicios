eb039f6f9c84d3513bb66358e64c2b2affed5b77
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2021-10-19 20:30:40 -0500

    yes

--- jacaScript/ejercicio7.js
+++ jacaScript/ejercicio7.js
@@ -29,13 +29,19 @@ function analizar(){
             if (Number.isInteger(num2)&& num1!=Number.isInteger(num1) ){
                 document.getElementById('titulo').innerHTML='El número'+num1 +' no es entero';
             }
+            if(num1!=Number.isInteger(num1)&&num2!=Number.isInteger(num2)){
+                document.getElementById('titulo').innerHTML='los numeros '+num1 +' y '+ num2 +' no son enteros';
+            }
         }
     }
     if(num1<0 && num2>0){
         if (Number.isInteger(num1)&&Number.isInteger(num2)){
             document.getElementById('titulo').innerHTML='El número '+num1+' No es positivo';
         }else{
-            document.getElementById('titulo').innerHTML='El número '+num1+' No es entero positivo y el número '+num2+' No es entero';
+            if(Number.isInteger(num1)&&num2!=Number.isInteger(num2)){
+                document.getElementById('titulo').innerHTML='El número '+num1+' No es positivo y el número '+num2+' No es entero positivo';
+            }
+           
         }
         
     }
@@ -43,7 +49,10 @@ function analizar(){
         if (Number.isInteger(num1) && Number.isInteger(num2)){
             document.getElementById('titulo').innerHTML='El número '+num2+' No es positivo';
         }else{
-            document.getElementById('titulo').innerHTML='El número '+num1+' No es positivo y el número '+num2+' No es entero positivo';
+            if(Number.isInteger(num1)&&num2!=Number.isInteger(num2)){
+                document.getElementById('titulo').innerHTML='El número '+num2+' No es positivo y el número '+num1+' No es entero positivo';
+            }
+            
         }
          
     }
