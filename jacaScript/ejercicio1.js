a4077242033d5e3ce51c34e4da73df108df05278
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2021-10-19 23:14:36 -0500

    div

--- jacaScript/ejercicio1.js
+++ jacaScript/ejercicio1.js
@@ -1,5 +1,5 @@
 function analizar(){
 let varCadena = document.getElementById('cadena').value;
 let resultado = varCadena.length; 
-document.getElementById('titulo').innerHTML = '<strong> La cantidad de caracteres es: </strong>'+resultado;
+document.getElementById('res').innerHTML = '<strong> La cantidad de caracteres es: </strong>'+resultado;
 }
