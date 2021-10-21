dab5be4de0bb9e30663be186bdc9c4ec8b2019c7
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2021-10-21 13:54:25 -0500

    js ejercicio7

--- jacaScript/ejercicio7.js
+++ jacaScript/ejercicio7.js
@@ -4,31 +4,31 @@ function analizar(){
 
     if(num1<0 &&num2<0){
         if (Number.isInteger(num1)&&Number.isInteger(num2)){
-            document.getElementById('titulo').innerHTML='los numeros'+num1 +' y '+ num2 +' No son positivos';
+            document.getElementById('res').innerHTML='los numeros'+num1 +' y '+ num2 +' No son positivos';
         }else{
-            document.getElementById('titulo').innerHTML='los numeros'+num1 +' y '+ num2 +' No son enteros positivos';
+            document.getElementById('res').innerHTML='los numeros'+num1 +' y '+ num2 +' No son enteros positivos';
         }
         if (Number.isInteger(num1)&& num2!=Number.isInteger(num2) ){
-            document.getElementById('titulo').innerHTML='El número'+num1 +' no es positivo y el número '+ num2 +'  no es entero positivo';
+            document.getElementById('res').innerHTML='El número'+num1 +' no es positivo y el número '+ num2 +'  no es entero positivo';
         }
         if (Number.isInteger(num2)&& num1!=Number.isInteger(num1) ){
-            document.getElementById('titulo').innerHTML='El número'+num1 +' no es entero positivo y el número '+ num2 +'  no es positivo';
+            document.getElementById('res').innerHTML='El número'+num1 +' no es entero positivo y el número '+ num2 +'  no es positivo';
         }
         
     }else{
         if (Number.isInteger(num1)&&Number.isInteger(num2)){
             if(Number.isInteger(num1/num2)){
-                document.getElementById('titulo').innerHTML='El número ' + num1+' es divisible entre el número '+num2;
+                document.getElementById('res').innerHTML='El número ' + num1+' es divisible entre el número '+num2;
             }else{
-                document.getElementById('titulo').innerHTML='El número ' + num1+' no es divisible entre el número '+num2;
+                document.getElementById('res').innerHTML='El número ' + num1+' no es divisible entre el número '+num2;
             }
         }else{
-            document.getElementById('titulo').innerHTML='los numeros '+num1 +' y '+ num2 +' no son enteros';
+            document.getElementById('res').innerHTML='los numeros '+num1 +' y '+ num2 +' no son enteros';
             if (Number.isInteger(num1) && num2!=Number.isInteger(num2) ){
-                document.getElementById('titulo').innerHTML='El número'+ num2 +'  no es entero';
+                document.getElementById('res').innerHTML='El número'+ num2 +'  no es entero';
             }
             if (num1!=Number.isInteger(num1) && Number.isInteger(num2) ){
-                document.getElementById('titulo').innerHTML='El número'+num1 +' no es entero';
+                document.getElementById('res').innerHTML='El número'+num1 +' no es entero';
             }
                 
             
@@ -36,28 +36,40 @@ function analizar(){
     }
     if(num1<0 && num2>0){
         if (Number.isInteger(num1)&&Number.isInteger(num2)){
-            document.getElementById('titulo').innerHTML='El número '+num1+' No es positivo';
+            document.getElementById('res').innerHTML='El número '+num1+' No es positivo';
         }else{
             if(Number.isInteger(num1)&&num2!=Number.isInteger(num2)){
-                document.getElementById('titulo').innerHTML='El número '+num1+' No es positivo y el número '+num2+' No es entero positivo';
+                document.getElementById('res').innerHTML='El número '+num1+' No es positivo y el número '+num2+' No es entero.';
             }
             if(num1!=Number.isInteger(num1)&&Number.isInteger(num2)){
-                document.getElementById('titulo').innerHTML='El número '+num1+' No es entero positivo';
+                document.getElementById('res').innerHTML='El número '+num1+' No es entero positivo';
+            }
+            if(num1!=Number.isInteger(num1)&& num2!=Number.isInteger(num2)){
+                document.getElementById('res').innerHTML='El número '+num1+' No es entero positivo y el número '+num2+' No es entero' ;
             }
            
         }
         
     }
-    if(num1>0 && num2 <0){
+    if(num1 >0 && num2 <0){
         if (Number.isInteger(num1) && Number.isInteger(num2)){
-            document.getElementById('titulo').innerHTML='El número '+num2+' No es positivo';
+            document.getElementById('res').innerHTML='El número '+num2+' No es positivo';
         }else{
-            if(Number.isInteger(num1)&&num2!=Number.isInteger(num2)){
-                document.getElementById('titulo').innerHTML='El número '+num2+' No es positivo y el número '+num1+' No es entero positivo';
+            //si no son enteros los dos numeros
+            if(Number.isInteger(num1)&&num2!=Number.isInteger(num2)){//el numero 1 entero y numero 2 diferente de entero y negativo
+                document.getElementById('res').innerHTML='El número '+num2+' No es entero positivo';
+            }else{
+                if(num1!=Number.isInteger(num1)&& Number.isInteger(num2)){
+                    document.getElementById('res').innerHTML='El número '+num1+' No es entero y el número '+num2 +' No es positivo';
+                }else{
+                    if(num1!=Number.isInteger(num1)&& num2!=Number.isInteger(num2)){
+                        document.getElementById('res').innerHTML='El número '+num1+' No es entero y el número '+num2+' No es entero positivo' ;
+                    }
+                }
+                
             }
-            if(num1!=Number.isInteger(num2)&&Number.isInteger(num1)){
-                document.getElementById('titulo').innerHTML='El número '+num2+' No es entero positivo';
-            }
+             
+            
             
         }
          
