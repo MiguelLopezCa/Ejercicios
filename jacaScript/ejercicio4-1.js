a193cbfcb77f87f088a38227f261fcae271cea80
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2021-10-21 10:26:56 -0500

    js ejer4

--- jacaScript/ejercicio4-1.js
+++ jacaScript/ejercicio4-1.js
@@ -8,15 +8,18 @@ function onClickCrear(){
 
 }
 function promedio(nn, aa,n1, n2, n3){
- let resul = (n1+n2+n3)/3;  
- if (resul<3){
-     document.getElementById('res').innerHTML ='Lo siento '+nn+ ', su nota es '+resul +' No pasaste la materia '+aa;
- }else if(resul>=3){
-    document.getElementById('res').innerHTML ='Felicitaciones '+nn+ ', su nota es '+resul+' Pasaste la materia '+aa;
- }else if(resul>5){
-    document.getElementById('res').innerHTML = 'la nota mas alta es de 5 Por favor verificar las notas'
- } else{
-    document.getElementById('res').innerHTML = 'las notas son invalidas'
- }
- alert("el resultado es:"+resul);
+   if(n1<0 ||n2<0 ||n3<0 || n1>5 ||n2>5 ||n3>5 ){
+      document.getElementById('res').innerHTML = 'Existe una o mas notas invalidas'
+   }else{
+      let resul = (n1+n2+n3)/3;  
+      if (resul<3){
+         document.getElementById('res').innerHTML ='Lo siento '+nn+ ', su nota es '+resul +' No pasaste la materia '+aa;
+     }
+      if(resul>=3){
+        document.getElementById('res').innerHTML ='Felicitaciones '+nn+ ', su nota es '+resul+' Pasaste la materia '+aa;
+     }
+      if(resul>5){
+        document.getElementById('res').innerHTML = 'la nota mas alta es de 5 Por favor verificar las notas'
+     } 
+   }
 }
